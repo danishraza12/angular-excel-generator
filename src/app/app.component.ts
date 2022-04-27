fb03bd4caecad2201016a9b39cb358fd4b8e7f59
Danish Raza <danish.raza@hrsgbpo.com>
2022-04-27 12:42:36 +0500

    Change object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,29 @@ export class AppComponent {
 
   dataForExcel: any = [];
 
+  // empPerformance = {
+  //   tableOne: [
+  //     { ID: 10011, NAME: "ABC", DEPARTMENT: "Sales", MONTH: "Jan", YEAR: 2020, SALES: 132412, CHANGE: 12, LEADS: 35, NEWCOLUMN: "Test Column 1" },
+  //     { ID: 10012, NAME: "ABC", DEPARTMENT: "Sales", MONTH: "Feb", YEAR: 2020, SALES: 232324, CHANGE: 2, LEADS: 443, NEWCOLUMN: "Test Column 2" },
+  //     { ID: 10013, NAME: "ABC", DEPARTMENT: "Sales", MONTH: "Mar", YEAR: 2020, SALES: 542234, CHANGE: 45, LEADS: 345, NEWCOLUMN: "Test Column 3" },
+  //     { ID: 10014, NAME: "ABC", DEPARTMENT: "Sales", MONTH: "Apr", YEAR: 2020, SALES: 223335, CHANGE: 32, LEADS: 234, NEWCOLUMN: "Test Column 4" },
+  //     { ID: 10015, NAME: "ABC", DEPARTMENT: "Sales", MONTH: "May", YEAR: 2020, SALES: 455535, CHANGE: 21, LEADS: 12, NEWCOLUMN: "Test Column 5" },
+  //   ],
+  //   tableTwo: [
+  //     { ID: 10011, NAME: "DEF", DEPARTMENT: "IT", MONTH: "Jan", YEAR: 2021, SALES: 132412, CHANGE: 12, LEADS: 35, NEWCOLUMN: "Test Column 1" },
+  //     { ID: 10012, NAME: "DEF", DEPARTMENT: "IT", MONTH: "Feb", YEAR: 2021, SALES: 232324, CHANGE: 2, LEADS: 443, NEWCOLUMN: "Test Column 2" },
+  //     { ID: 10013, NAME: "DEF", DEPARTMENT: "IT", MONTH: "Mar", YEAR: 2021, SALES: 542234, CHANGE: 45, LEADS: 345, NEWCOLUMN: "Test Column 3" },
+  //     { ID: 10014, NAME: "DEF", DEPARTMENT: "IT", MONTH: "Apr", YEAR: 2021, SALES: 223335, CHANGE: 32, LEADS: 234, NEWCOLUMN: "Test Column 4" },
+  //     { ID: 10015, NAME: "DEF", DEPARTMENT: "IT", MONTH: "May", YEAR: 2021, SALES: 455535, CHANGE: 21, LEADS: 12, NEWCOLUMN: "Test Column 5" },
+  //   ]
+  // };
+  
   empPerformance = [
-    { ID: 10011, NAME: "ABC", DEPARTMENT: "Sales", MONTH: "Jan", YEAR: 2020, SALES: 132412, CHANGE: 12, LEADS: 35, NEWCOLUMN: "Test Column 1" },
-    { ID: 10012, NAME: "ABC", DEPARTMENT: "Sales", MONTH: "Feb", YEAR: 2020, SALES: 232324, CHANGE: 2, LEADS: 443, NEWCOLUMN: "Test Column 2" },
-    { ID: 10013, NAME: "ABC", DEPARTMENT: "Sales", MONTH: "Mar", YEAR: 2020, SALES: 542234, CHANGE: 45, LEADS: 345, NEWCOLUMN: "Test Column 3" },
-    { ID: 10014, NAME: "ABC", DEPARTMENT: "Sales", MONTH: "Apr", YEAR: 2020, SALES: 223335, CHANGE: 32, LEADS: 234, NEWCOLUMN: "Test Column 4" },
-    { ID: 10015, NAME: "ABC", DEPARTMENT: "Sales", MONTH: "May", YEAR: 2020, SALES: 455535, CHANGE: 21, LEADS: 12, NEWCOLUMN: "Test Column 5" },
+    { ID: 10011, NAME: "A", DEPARTMENT: "Sales", MONTH: "Jan", YEAR: 2020, SALES: 132412, CHANGE: 12, LEADS: 35 },
+    { ID: 10012, NAME: "A", DEPARTMENT: "Sales", MONTH: "Feb", YEAR: 2020, SALES: 232324, CHANGE: 2, LEADS: 443 },
+    { ID: 10013, NAME: "A", DEPARTMENT: "Sales", MONTH: "Mar", YEAR: 2020, SALES: 542234, CHANGE: 45, LEADS: 345 },
+    { ID: 10014, NAME: "A", DEPARTMENT: "Sales", MONTH: "Apr", YEAR: 2020, SALES: 223335, CHANGE: 32, LEADS: 234 },
+    { ID: 10015, NAME: "A", DEPARTMENT: "Sales", MONTH: "May", YEAR: 2020, SALES: 455535, CHANGE: 21, LEADS: 12 },
   ];
 
   constructor(public ete: ExportExcelService) { }
